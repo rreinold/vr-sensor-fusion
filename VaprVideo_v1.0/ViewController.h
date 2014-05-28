78383ee7fd98ab86745c158b5c2776a6e0c731ab
rreinold <rreinold@gmail.com>
2014-05-28 10:44:47 -0400

    Initial Commit

---
+++ VaprVideo_v1.0/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VaprVideo_v1.0
+//
+//  Created by Robert Reinold on 5/28/14.
+//  Copyright (c) 2014 Robert Reinold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VaprVideo_v1.0/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VaprVideo_v1.0
+//
+//  Created by Robert Reinold on 5/28/14.
+//  Copyright (c) 2014 Robert Reinold. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
